81faa857e4215ce935997e7669f2d10d048d6953
Wesley Alves <wesandradealves@gmail.com>
2025-04-26 01:08:11 -0300

    bugfix(profit): fix para nameError: name 'schemas' is not defined e AttributeError: 'dict' object has no attribute 'total_price'

--- app/utils/profit.py
+++ app/utils/profit.py
@@ -1,6 +1,14 @@
 from app.schemas.schemas import Sale
+from app.schemas import schemas
 
 def calculate_profit(sale):
-    sale_dict = sale.__dict__ 
-    sale_dict['profit'] = sale.total_price  
-    return sale_dict
+    profit = sale.total_price * 0.2  
+    sale_with_profit = schemas.SaleWithProfit(
+        id=sale.id,
+        product_id=sale.product_id,
+        quantity=sale.quantity,
+        total_price=sale.total_price,
+        date=sale.date,
+        profit=profit
+    )
+    return sale_with_profit
