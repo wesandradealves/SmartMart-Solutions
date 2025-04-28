8a4a1cea9fdb0661ce9d64562d6389450e4edc24
Wesley Alves <wesandradealves@gmail.com>
2025-04-28 19:02:07 -0300

    bugfix(csv): fix para propriedades nao sendo importadas no csv

--- app/services/csv_importer.py
+++ app/services/csv_importer.py
@@ -4,7 +4,13 @@ from app.models import models
 def import_products_csv(file, db):
     df = pd.read_csv(file.file)
     for _, row in df.iterrows():
-        product = models.Product(name=row["name"], price=row["price"], category_id=row["category_id"])
+        product = models.Product(
+            name=row["name"],
+            description=row.get("description"),  
+            price=row["price"],
+            category_id=row["category_id"],
+            brand=row.get("brand") 
+        )
         db.add(product)
     db.commit()
     return {"message": "Produtos importados com sucesso."}
